085ceb854b82d839888c033956ffd177410bd1cc
Frank Lavery <frank.lavery@westnet.com.au>
2022-03-27 13:29:51 +0800

    Functionality complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,51 @@
+// Variables linking to DOM elements
 const frmSearchEl = document.querySelector("#city-search");
 const iptSearchCityEl = document.getElementById("selected-city");
 const txtCurrentPlace = document.getElementById("current-place");
 const lstCurrentDayEl = document.getElementById("current-day-result");
+const lstPreviousSearchEl = document.getElementById("prev-search");
+const contForecastEl = document.getElementById("fcast-container");
 
-
+// Global variables and assignments
 const token = config.API_Token;
 const key = config.API_Key;
-const arryDaily = [];
+const arryNameStore = [];
 
+// Retrieve previously searched cities for selection by User if desired
+const writeSavedCities = function () {
+  let cityList = JSON.parse(localStorage.getItem("savedCity"));
+  if (cityList) {
+    for (i = 0; i < cityList.length; i++) {
+      arryNameStore[i] = cityList[i];
+      const btnPrevCity = document.createElement('button');
+      btnPrevCity.textContent = cityList[i];
+      btnPrevCity.setAttribute('name', cityList[i]);
+      lstPreviousSearchEl.appendChild(btnPrevCity);
+    }
+  }
+  return;
+}
+
+// Obtain weather data if previously searched city is selected
+const prevCitySelected = function (event) {
+  event.preventDefault();
+  // Get city coordinates
+  getCityCoords(event.target.getAttribute('name'))
+    .then(function (data) {
+      let txtCity = `${data[0].name}, ${data[0].country}`;
+      txtCurrentPlace.textContent = data[0].name + ", " + data[0].country;
+
+      // Using city coordinates, call openweather API and get weather data
+      getWeather(data[0].lon, data[0].lat)
+        .then(function (weatherData) {
+          extractWeather(weatherData);
+        })
+    })
+  return;
+}
+
+// Call to Geocode API to get city coordinates
 const getCityCoords = function (city) {
-  
   const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`
 
   return fetch(apiUrl)
@@ -18,9 +54,8 @@ const getCityCoords = function (city) {
     });
 }
 
+// call to openweathermap to get weather by coordinates
 const getWeather = function (lon, lat) {
-  console.log(`long is ${lon} and lat is ${lat}`);
-
   const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${key}&units=metric`;
 
   return fetch(apiUrl)
@@ -30,41 +65,66 @@ const getWeather = function (lon, lat) {
     });
 }
 
+// Function to render the five day forecast to the web-page
+const buildFiveDay = function (fcastObj) {
+  contForecastEl.textContent = "";
 
-const buildDailyForecast = function(weatherData) {
+  for (i = 0; i < 5; i++) {
+    const card = document.createElement('div');
+    const cardTitle = document.createElement('h3');
+    const cardImage = document.createElement('img');
+    const cardList = document.createElement('ul');
+
+    cardTitle.textContent = fcastObj[i].date;
+    cardImage.src = `https://openweathermap.org/img/wn/${fcastObj[i].iconCode}@2x.png`;
+    card.appendChild(cardTitle);
+    card.appendChild(cardImage);
+
+    const liTempMax = document.createElement('li');
+    liTempMax.textContent = "Temp Max: " + fcastObj[i].tempMax + "°C";
+    const liTempMin = document.createElement('li');
+    liTempMin.textContent = "Temp Min: " + fcastObj[i].tempMin + "°C";
+    const liWind = document.createElement('li');
+    liWind.textContent = "Wind:  " + fcastObj[i].wind + "km/hour";
+    const liHumidity = document.createElement('li');
+    liHumidity.textContent = "Humidity:  " + fcastObj[i].humidity + "%";
   
-  // const currentDayIcon = `https://openweathermap.org/img/wn/10d@2x.png`;
-
-  const currentDay = {
-    date: moment.unix(weatherData.current.dt).format('dddd, MMMM Do, YYYY h:mm:ss A'),
-    temp: weatherData.current.temp,
-    uvi: weatherData.current.uvi,
-    iconCode: weatherData.current.weather[0].icon,
-    wind: weatherData.current.wind_speed,
-    humidity: weatherData.current.humidity,
+    // Append to card list
+    cardList.appendChild(liTempMax);
+    cardList.appendChild(liTempMin);
+    cardList.appendChild(liWind);
+    cardList.appendChild(liHumidity);
+    card.appendChild(cardList);
+    // Append to container on web-page
+    contForecastEl.appendChild(card);
   }
-  
+  return;
+}
+
+// Function to render the current day information to the web-page
+const buildCurrentDay = function (today) {
   // Log current Days Weather to Screen
-  // Clear previous list
+  // Clear previous data, if any
   lstCurrentDayEl.textContent = "";
   // Create list elements
-  txtCurrentPlace.textContent = txtCurrentPlace.textContent + " (" + currentDay.date + ")";
+  txtCurrentPlace.textContent = txtCurrentPlace.textContent + " (" + today.date + ")";
+  const iconImage = document.createElement('img');
+  iconImage.src = `https://openweathermap.org/img/wn/${today.iconCode}@2x.png`;
+  txtCurrentPlace.appendChild(iconImage);
   const liTemp = document.createElement('li');
-  liTemp.textContent = "Temp: " + currentDay.temp + "°C";
+  liTemp.textContent = "Temp: " + today.temp + "°C";
   const liUvi = document.createElement('li');
-  liUvi.textContent = "UVI:  " + currentDay.uvi;
+  liUvi.textContent = "UVI:  " + today.uvi;
   const liWind = document.createElement('li');
-  liWind.textContent = "Wind:  " + currentDay.wind + "km/hour";
+  liWind.textContent = "Wind:  " + today.wind + "km/hour";
   const liHumidity = document.createElement('li');
-  liHumidity.textContent = "Humidity:  " + currentDay.humidity + "%";
-  const liIcon = document.createElement('li');
-  liIcon.textContent = "IconCode:  " + currentDay.iconCode;
+  liHumidity.textContent = "Humidity:  " + today.humidity + "%";
 
   // Add Color to UVI Index
-  if (currentDay.uvi < 3) {
+  if (today.uvi < 3) {
     liUvi.style.background = "green";
-  } else if (currentDay.uvi < 8){
-      liUvi.style.background = "yellow";
+  } else if (today.uvi < 8) {
+    liUvi.style.background = "yellow";
   } else {
     liUvi.style.background = "red";
   }
@@ -74,76 +134,73 @@ const buildDailyForecast = function(weatherData) {
   lstCurrentDayEl.appendChild(liWind);
   lstCurrentDayEl.appendChild(liHumidity);
   lstCurrentDayEl.appendChild(liUvi);
-  lstCurrentDayEl.appendChild(liIcon);
+  return;
+}
 
-  // reset the forecast array to prevent appending
-  // may not need this if converting to cards, but will need a method to refresh if a different city is chosen
-  arryDaily.length = 0;
+// Extract the required weather information from the returned data
+const extractWeather = function(weatherData) {
+  // Object to hold relevant information for the current day
+  const currentDay = {
+    date: moment.unix(weatherData.current.dt).format('dddd, MMMM Do, YYYY h:mm A'),
+    temp: weatherData.current.temp,
+    uvi: weatherData.current.uvi,
+    iconCode: weatherData.current.weather[0].icon,
+    wind: weatherData.current.wind_speed,
+    humidity: weatherData.current.humidity,
+  }
+  
+  // Array containing and object for each day of forecast.  Object contains the relevant information for that day
+  const arryDaily = [];
 
   for (i = 1; i < 6; i++) {
     tempObj = {
-      date: moment.unix(weatherData.daily[i].dt),
+      date: moment.unix(weatherData.daily[i].dt).format('dddd, MMMM Do, YYYY'),
       tempMax: weatherData.daily[i].temp.max,
       tempMin: weatherData.daily[i].temp.min,
       iconCode: weatherData.daily[i].weather[0].icon,
       wind: weatherData.daily[i].wind_speed,
       humidity: weatherData.daily[i].humidity,
     }
-    console.log("Daily Index information",i, tempObj.iconCode);
     arryDaily.push(tempObj);
   }
+  // Render the data to the web-page
+  buildCurrentDay(currentDay);
+  buildFiveDay(arryDaily);
+  return;
 }
   
+// Search for a new city and add to local storage for future previously searched lists
 const searchByCity = function (event) {
   event.preventDefault();
   const cityName = iptSearchCityEl.value.trim();
-  console.log(cityName);
-  
-
-  // Place into local storage
-
-  // TO DO  If error, advise user that City cannot be found
-
+    
   // Call openweather API to get coordinates (long/lat) of City
-  getCityCoords(cityName)
-    .then(function(data) {
-      console.log(`city ${data[0].name}`)
-      txtCurrentPlace.textContent = data[0].name + ", "+ data[0].country;
-      
-      // Using coordinates of City, call openweather API and get weather by long/lat
-      getWeather(data[0].lon, data[0].lat)
-        .then(function (weatherData) {
-          buildDailyForecast(weatherData);
-          
-        })
-    })
+  if (cityName != "" || null) {
+    getCityCoords(cityName)
+      .then(function (data) {
+        let txtCity = `${data[0].name}, ${data[0].country}`;
+        txtCurrentPlace.textContent = data[0].name + ", " + data[0].country;
+              
+        // Using coordinates of City, call openweather API and get weather by long/lat
+        getWeather(data[0].lon, data[0].lat)
+          .then(function (weatherData) {
+            // Write city name to local storage
+            arryNameStore.push(txtCity);
+            localStorage.setItem("savedCity", JSON.stringify(arryNameStore));
+
+            extractWeather(weatherData);
+          })
+      })
+  }
   return;
 }
-
  
+// On starting, retrieve previous cities
+writeSavedCities();
 
-
-// Daily Weather:  Display City Name (Date in brackets) as a heading, with weather icon alongside
-// Below display the weather, wind, Humidity and UV Index.  UV Index to be color-coded
-
-// Below Daily Weather, display five day forecast: Date, icon, temp, wind, humidity
-
-// If new search, place selected city into local storage
-
-
-// on starting, retrieve previous cities searched from local storage and display beneath the search input, as selectable buttons, with a line divider
-
-// Work out how to keep API key safe - call openweather requires an API key
-
-
+// Event Listener:  User has entered a City to search.
+// Will call function to search for the City entered by the User.  If found, will display the weather
+frmSearchEl.addEventListener('submit', searchByCity);
 
 // Event Listener:  User has clicked on a previously searched city (could have a listener on whole block, and use button names as parameters)
-// If a previously searched city is called, will display the weather for that city
-
-
-
-// Event Listener:  User has entered a City to search or clicked on a previously searched city
-// Will call function to search for the City entered by the User.  If found, will display the weather
-// TO DO If not found, will advise that City could not be found
-
-frmSearchEl.addEventListener('submit', searchByCity);
+lstPreviousSearchEl.addEventListener('click', prevCitySelected);
